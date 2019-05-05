1f8b3884e8772bf80abe2af16d580b8a8517746e
brianf21 <brian.faris@gmail.com>
2019-05-05 16:27:15 -0400

    Create final.sh

---
+++ final.sh
@@ -0,0 +1,19 @@
+ip address show eth0
+ip route list 0.0.0.0/0
+cat /etc/sysconfig/network-scripts/ifcfg-eth0 | grep ONBOOT
+cat /etc/sysconfig/network-scripts/ifcfg-static | grep ONBOOT
+nmcli con show
+hostnamectl
+cat /etc/hosts
+systemctl static httpd
+systemctl list-unit-files | grep httpd
+cat /var/www/html/index.html
+ssh rhs2 << EOF
+ip address show eth0
+ip route list 0.0.0.0/0
+cat /etc/sysconfig/network-scripts/ifcfg-eth0 | grep ONBOOT
+cat /etc/sysconfig/network-scripts/ifcfg-static | grep ONBOOT
+nmcli con show
+hostnamectl
+cat /etc/hosts
+EOF
