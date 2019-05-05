15dbf1f44378a8324a102621d527272be09229dd
brianf21 <brian.faris@gmail.com>
2019-05-05 17:07:56 -0400

    Update final.sh

--- final.sh
+++ final.sh
@@ -12,7 +12,7 @@ systemctl static httpd
 systemctl list-unit-files | grep httpd
 cat /var/www/html/index.html
 ls -l ~
-ssh roo@rhs2
+ssh roo@rhs2 << EOF
 echo ooooooooooooooooooooooooooooooooooooooo
 echo Redhat Server 2
 echo ooooooooooooooooooooooooooooooooooooooo
@@ -24,3 +24,4 @@ nmcli con show
 hostnamectl
 cat /etc/hosts
 ls -l ~
+EOF
