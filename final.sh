617bcb6b67e41da61185540ac0b48e0400e253c1
brianf21 <brian.faris@gmail.com>
2019-05-05 16:29:45 -0400

    Update final.sh

--- final.sh
+++ final.sh
@@ -8,6 +8,7 @@ cat /etc/hosts
 systemctl static httpd
 systemctl list-unit-files | grep httpd
 cat /var/www/html/index.html
+ls -l ~
 ssh rhs2 << EOF
 ip address show eth0
 ip route list 0.0.0.0/0
@@ -16,4 +17,5 @@ cat /etc/sysconfig/network-scripts/ifcfg-static | grep ONBOOT
 nmcli con show
 hostnamectl
 cat /etc/hosts
+ls -l ~
 EOF
